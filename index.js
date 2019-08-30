cb96aa41367c770376f3502bed15c5dd1db9d11e
Mic Black <mic@micslab.com>
2019-08-30 16:17:56 +1000

    Example of using methods and the created() event to change the salutation based on the time of day

--- index.js
+++ index.js
@@ -33,9 +33,26 @@ const vm = new window.Vue({
 
 	methods: {
 
+		salutationByTimeOfDay() {
+			let hour = new Date().getHours()
+			let response = 'Hello'
+
+			if (hour < 12) {
+			  response = 'Good morning'
+			} else if (hour < 18) {
+			  response = 'Good afternoon'
+			} else {
+			  response = 'Good evening'
+			}
+
+			return response
+		}
+
 	},
 
 	created() {
+
+		this.salutation = this.salutationByTimeOfDay()
 
 	}
 
